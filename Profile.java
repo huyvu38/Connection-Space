a769f63f089faa3184d037f05e3ee55385e86ab3
huyvu38 <vu38@purdue.edu>
2024-03-29 09:36:08 -0400

    Merge remote-tracking branch 'origin/main'

--- Profile.java
+++ Profile.java
@@ -13,8 +13,8 @@ public class Profile {
     private String nationality;
     private String job;
     private String hobby;
-    public Profile (String useerName, int age, String gender, String nationality, String job, String hobby) {
-        this.userName = useerName;
+    public Profile (String userName, int age, String gender, String nationality, String job, String hobby) {
+        this.userName = userName;
         this.age = age;
         this.gender = gender;
         this.nationality = nationality;
