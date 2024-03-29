690a05a8fb7527b3b822248034932aacdde02688
huyvu38 <vu38@purdue.edu>
2024-03-29 19:29:15 -0400

    Merge remote-tracking branch 'origin/main'

--- Database.java
+++ Database.java
@@ -10,26 +10,26 @@ import java.util.ArrayList;
  */
 public class Database {
     private String allUserAccountFile;
-    private String allUserProfileFile;
-    private ArrayList<Profile> allUserProfile;
     private ArrayList<UserAccount> allUserAccount;
     private String databaseOutput;
-    public Database (String allUserAccountFile, String allUserProfileFile, String databaseOutput) {
+    public Database (String allUserAccountFile, String databaseOutput) {
         this.allUserAccountFile = allUserAccountFile;
-        this.allUserProfileFile = allUserProfileFile;
-        this.databaseOutput = databaseOutput;
-        this.allUserProfile = new ArrayList<Profile>();
         this.allUserAccount = new ArrayList<UserAccount>();
+        this.databaseOutput = databaseOutput;
     }
     public boolean readAllUserAccount(String allUserAccountFile) {}
     public boolean readAllUserProfile(String allUserAccountFile) {}
     public boolean userCreateAccount(UserAccount newUserAccount) {}
     public boolean userDeleteAccount(UserAccount deleteUserAccount) {}
-    public boolean userEditAccount(UserAccount userEditAccount)
-    //What if the user create account
-    //What if the user delete account
-    //What if the user change anything in Profile.java or UserAccount.java
-    public boolean output(String databaseOutput)
+    //If user edit username ->
+    //(need to use updateFriendUserName & updateBlockUserName) cuz that user may be in another friendlist or blocklist
+
+
+    //If the user edit any thing in the profile outside username
+    public boolean userEditInformation(UserAccount userEditAccount)
+
+    //Store back to the allUserAccount.txt
+    public boolean output(String databaseOutput) {}
 
 
 }
