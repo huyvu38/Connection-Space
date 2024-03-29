6c77f8b912ff7920b83e6dece60b756467559df1
huyvu38 <vu38@purdue.edu>
2024-03-28 22:30:56 -0400

    Merge remote-tracking branch 'origin/main'

--- Profile.java
+++ Profile.java
@@ -6,6 +6,57 @@
  * @author Gabe Turner, Archie Baldocchi, Huy Vu, Yanxin Yu, Zander Unger, L22
  * @version 28 March 2024
  */
-public class Profile{
+public class Profile {
+    private int age;
+    private String gender;
+    private String nationality;
+    private String job;
+    private String hobby;
+    public Profile (int age, String gender, String nationality, String job, String hobby) {
+        this.age = age;
+        this.gender = gender;
+        this.nationality = nationality;
+        this.job = job;
+        this.hobby = hobby;
+    }
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
 }
