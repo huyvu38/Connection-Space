144b1d8e48b664149cf2b9c5c82f7341c2e3c023
huyvu38 <vu38@purdue.edu>
2024-03-29 13:12:21 -0400

    Merge remote-tracking branch 'origin/main'

--- Database.java
+++ Database.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Team Project
  *
@@ -7,5 +9,23 @@
  * @version 28 March 2024
  */
 public class Database {
+    private String databaseOutput;
+    private ArrayList<Profile> allUserProfile;
+    private ArrayList<UserInformation> allUserInformation;
+    private String allUserProfileFile;
+    private String allUserInformationFile;
+    public Database (String allUserInformationFile, String allUserProfileFile, String databaseOutput) {
+        this.allUserInformationFile = allUserInformationFile;
+        this.allUserProfileFile = allUserProfileFile;
+        this.databaseOutput = databaseOutput;
+        this.allUserProfile = new ArrayList<>();
+        this.allUserInformation = new ArrayList<>();
+    }
+    public boolean readAllUserInformation(String allUserInformationFile) {}
+    public boolean readAllUserProfile(String allUserProfileFile) {}
+    public boolean updateAllUserInformation(){}
+    public boolean updateAllUserProfile(){}
+    public boolean output(String databaseOutput)
+
 
 }
