f00c269cc4090d1f627c5981a411a5f5992438ff
huyvu38 <vu38@purdue.edu>
2024-03-29 01:28:16 -0400

    Merge remote-tracking branch 'origin/main'

--- Message.java
+++ Message.java
@@ -7,4 +7,12 @@
  * @version 28 March 2024
  */
 public class Message {
+    private UserInformation sender;
+    private UserInformation receiver;
+    private String content;
+    public Message (UserInformation sender, UserInformation receiver, String content) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.content = content;
+    }
 }
