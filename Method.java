ac5e37ad7bf97eef29f7efbf1dba2b9fe84cfdb5
huyvu38 <vu38@purdue.edu>
2024-03-30 09:43:56 -0400

    Merge remote-tracking branch 'origin/main'

--- Method.java
+++ Method.java
@@ -17,57 +17,70 @@ public class Method {
         }
         return false;
     }
-    public boolean inFriendList (ArrayList<Profile> friendList, String userName) {
-        for (Profile eachProfile : friendList) {
-            if (eachProfile.getUserName().equals(userName)) {
-                return true;
+    public boolean inFriendList (ArrayList<Profile> allUserList, ArrayList<String> friendList, String userName) {
+        if (isValidUserName(allUserList, userName)) {
+            for (String eachFriend : friendList) {
+                if (eachFriend.equals(userName)) {
+                    return true;
+                }
             }
         }
         return false;
     }
-    public boolean inBlockList (ArrayList<Profile> blockList, String userName) {
-        for (Profile eachProfile : blockList) {
-            if (eachProfile.getUserName().equals(userName)) {
-                return true;
+    public boolean inBlockList (ArrayList<Profile> allUserList, ArrayList<String> blockList, String userName) {
+        if (isValidUserName(allUserList, userName)) {
+            for (String eachBlockUser : blockList) {
+                if (eachBlockUser.equals(userName)) {
+                    return true;
+                }
             }
         }
         return false;
     }
 
-    public boolean addFriend (ArrayList<Profile> friendList, ArrayList<Profile> blockList, String userName) {
-        if (inBlockList(blockList, userName)) {
-            return false; //Already block user
-        }
-        if (inFriendList(friendList, userName)) {
-            return false; // already in the list
-        }
-        return true;
-    }
-    public boolean removeFriend (ArrayList<Profile> friendList, ArrayList<Profile> blockList, String userName) {
-        if (inBlockList(blockList, userName)) {
-            return false;
-        }
-        if (inFriendList(friendList, userName)) {
-            return true; //That userName in the list so can remove friend
-        }
-        return false;
-    }
-    public boolean blockUser (ArrayList<Profile> blockList, ArrayList<Profile> allUserList, String userName) {
-        if (inBlockList(blockList, userName)) {
-            return false; //Already block
-        }
+    public boolean addFriend (ArrayList<Profile> allUserList, ArrayList<String> friendList, ArrayList<String> blockList, String userName) {
         if (isValidUserName(allUserList, userName)) {
-            return true; //Can block that user
+            if (inBlockList(allUserList, blockList, userName)) {
+                return false; //Already block user
+            }
+            if (inFriendList(allUserList, friendList, userName)) {
+                return false; // already in the friend list
+            } else {
+                return true;
+            }
         }
         return false;
     }
-    public boolean searchUser (ArrayList<Profile> blockList, ArrayList<Profile> allUserList, String word) {
+    public boolean removeFriend (ArrayList<Profile> allUserList, ArrayList<String> friendList, ArrayList<String> blockList, String userName) {
+        if (isValidUserName(allUserList, userName)) {
+            if (inBlockList(allUserList, blockList, userName)) {
+                return false; //Already in block list
+            }
+            if (inFriendList(allUserList, friendList, userName)) {
+                return true; //That userName in the list so can remove friend
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+    public boolean blockUser (ArrayList<Profile> allUserList, ArrayList<String> blockList, String userName) {
+        if (isValidUserName(allUserList, userName)) {
+            if (inBlockList(allUserList, blockList, userName)) {
+                return false; //Already block
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean searchUser (ArrayList<Profile> allUserList, ArrayList<String> blockList, String word) {
         int count = 0;
         for (Profile eachProfile : allUserList) {
             //Check all username that contain the word
             if (eachProfile.getUserName().contains(word)) {
                 //Do not display username in block list
-                if (inBlockList(blockList, word) == false) {
+                if (inBlockList(allUserList, blockList, word) == false) {
                     count = count + 1;
                     System.out.println(count + ". " + eachProfile.getUserName());
                 }
