c1f8e65b3c054745ac5c8d643d2b88efc10e8b49
Yanxin1107 <128647034+Yanxin1107@users.noreply.github.com>
2024-03-30 13:01:41 -0400

    111

--- LogIn.java
+++ LogIn.java
@@ -1,17 +1,14 @@
 import java.util.ArrayList;
 
 public class LogIn {
-
-
-    public boolean createAccount (ArrayList<Profile> allUserList, String name, String password) {
-        if (isValidUserName(allUserList,name)) {
-
+    public boolean checkPasswordLength (String password) {
+        if (password.length() == 6) {
             return true;
-
         }
 
         return false;
     }
+
     public boolean isValidUserName (ArrayList<Profile> allUserList, String userName) {
         for (Profile eachProfile : allUserList) {
             if (eachProfile.getUserName().equals(userName)) {
@@ -21,9 +18,44 @@ public class LogIn {
         return false;
     }
 
+    public boolean createAccount (Database database, Profile newProfile) {
+        if (isValidUserName(database.getAllUserProfile(),newProfile.getUserName())
+                && (checkPasswordLength(newProfile.getPassword()))) {
+            ArrayList<Profile> userList = database.getAllUserProfile();
+            ArrayList<UserAccount> userAccList = database.getAllUserAccount();
+
+            userList.add(newProfile);
+            userAccList.add(new UserAccount(newProfile));
+
+            database.setAllUserProfile(userList);
+            database.setAllUserAccount(userAccList);
+
+        }
+        return false;
+
+    }
+
+
     //Check if user name is already exist first
     //Check password is over than 6 character
 
-    //public boolean deleteAccount (String userName, String password) {}
+    public boolean deleteAccount (ArrayList<Profile> allUserList, String userName, String password) {
+        if (isValidUserName(database.getAllUserProfile(),newProfile.getUserName())
+                && (checkPasswordLength(newProfile.getPassword()))) {
+            ArrayList<Profile> userList = database.getAllUserProfile();
+            ArrayList<UserAccount> userAccList = database.getAllUserAccount();
+
+            userList.add(newProfile);
+            userAccList.add(new UserAccount(newProfile));
+
+            database.setAllUserProfile(userList);
+            database.setAllUserAccount(userAccList);
+
+        }
+        return false;
+
+    }
+        return isValidUserName(allUserList,userName) && checkPasswordLength(password);
+    }
     //public boolean loginAccount (String userName, String password) {}
 }
