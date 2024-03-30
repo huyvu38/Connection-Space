570f6125f4266c3be2336774bb06792788f7faea
huyvu38 <vu38@purdue.edu>
2024-03-30 00:23:10 -0400

    Merge remote-tracking branch 'origin/main'

--- ApplicationControlFlow.java
+++ ApplicationControlFlow.java
@@ -11,20 +11,20 @@ public class ApplicationControlFlow {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hi, welcome to the social media platform");
-        boolean choice = true;
-        while (choice) {
+        boolean input = true;
+        while (input) {
             System.out.println("What would you like to do?");
             System.out.println("1. Create account");
             System.out.println("2. Log In");
             System.out.println("3. Exit the app");
-            String userChoice = scanner.nextLine();
-            if (userChoice.equals("1")) {
+            String userInput = scanner.nextLine();
+            if (userInput.equals("1")) {
                 System.out.println("Enter your username");
                 String userName = scanner.nextLine();
                 System.out.println("Enter your password");
                 String password = scanner.nextLine();
                 int newAge = 0;
-                while (choice) {
+                while (input) {
                     System.out.println("Enter your age");
                     String age = scanner.nextLine();
                     try {
@@ -32,7 +32,7 @@ public class ApplicationControlFlow {
                         if (newAge <= 0) {
                             System.out.println("Please enter a valid number");
                         } else {
-                            choice = false;
+                            if input = false;
                         }
                     } catch (NumberFormatException e) {
                         System.out.println("Please enter a valid number");
@@ -50,8 +50,8 @@ public class ApplicationControlFlow {
                 UserAccount newUserAccount = new UserAccount(newProfile);
                 System.out.println("Create account success");
                 System.out.println("You have to log in again");
-                choice = true;
-            } else if (userChoice.equals("2")) {
+                input = true;
+            } else if (userInput.equals("2")) {
                 Database database = new Database("allUserAccount.txt", "allUserProfile.txt");
                 System.out.println("Enter your username");
                 String userName = scanner.nextLine();
@@ -68,17 +68,69 @@ public class ApplicationControlFlow {
                 System.out.println("7. Unblock friend");
                 System.out.println("8. Send message");
                 System.out.println("9. Log out");
-                userChoice = scanner.nextLine();
-                if (userChoice.equals("1")) {
+                userInput = scanner.nextLine();
+                if (userInput.equals("1")) {
                     for (Profile userProfile : database.getAllUserProfile()) {
-                        if (userProfile.getUserName().equals(userChoice)) {
+                        if (userProfile.getUserName().equals(userName)) {
                             System.out.println(userProfile);
                         }
                     }
-                } else if (userChoice.equals("2")) {
+                } else if (userInput.equals("2")) {
                     System.out.println("Which information do you want to edit");
-                    System.out.println();
-
+                    System.out.println("1. Username");
+                    System.out.println("2. Password");
+                    System.out.println("3. Age");
+                    System.out.println("4. Gender");
+                    System.out.println("5. Nationality");
+                    System.out.println("6. Job");
+                    System.out.println("7. Hobby");
+                    userInput = scanner.nextLine();
+                    if (userInput.equals("1")) {
+                        System.out.println("Enter your new username");
+                        userInput = scanner.nextLine();
+                        for (Profile userProfile : database.getAllUserProfile()) {
+                            if (userProfile.getUserName().equals(userName)) {
+                                userProfile.setUserName(userInput);
+                            }
+                        }
+                    } else if (userInput.equals("2")) {
+                        System.out.println("Enter your new password");
+                        userInput = scanner.nextLine();
+                        for (Profile userProfile : database.getAllUserProfile()) {
+                            if (userProfile.getUserName().equals(userName)) {
+                                userProfile.setPassword(userInput);
+                            }
+                        }
+                    } else if (userInput.equals("3")) {
+                        int newAge = 0;
+                        while (input) {
+                            System.out.println("Enter your age");
+                            String age = scanner.nextLine();
+                            try {
+                                newAge = Integer.parseInt(age);
+                                if (newAge <= 0) {
+                                    System.out.println("Please enter a valid number");
+                                } else {
+                                    input = false;
+                                }
+                            } catch (NumberFormatException e) {
+                                System.out.println("Please enter a valid number");
+                            }
+                        }
+                        for (Profile userProfile : database.getAllUserProfile()) {
+                            if (userProfile.getUserName().equals(userName)) {
+                                userProfile.setAge(newAge);
+                            }
+                        }
+                    } else if (userInput.equals("4")) {
+                        System.out.println("Enter your new password");
+                        userInput = scanner.nextLine();
+                        for (Profile userProfile : database.getAllUserProfile()) {
+                            if (userProfile.getUserName().equals(userName)) {
+                                userProfile.setPassword(userInput);
+                            }
+                        }
+                    }
                 } else if (userChoice.equals("3")) {
                     System.out.println("What information do you want to edit");
                     System.out.println("1. Username");
