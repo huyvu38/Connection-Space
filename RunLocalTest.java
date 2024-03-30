dbd09f84ee659626a23a9ae06ef8cb42ee5755c2
abaldocchi9 <164439258+abaldocchi9@users.noreply.github.com>
2024-03-30 12:05:16 -0400

    new message

---
+++ RunLocalTest.java
@@ -0,0 +1,84 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.RunWith;
+import org.junit.runner.notification.Failure;
+
+import java.lang.reflect.Modifier;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+/**
+ * Team Project
+ * <p>
+ * Test case class.
+ *
+ * @author Gabe Turner, Archie Baldocchi, Huy Vu, Yanxin Yu, Zander Unger, L22
+ * @version 28 March 2024
+ */
+
+@RunWith(Enclosed.class)
+public class RunLocalTest {
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(TestCase.class);
+        if (result.wasSuccessful()) {
+            System.out.println("Excellent - Test ran successfully");
+        } else {
+            for (Failure failure : result.getFailures()) {
+                System.out.println(failure.toString());
+            }
+        }
+    } // end of main
+
+    @RunWith(Enclosed.class)
+    public static class TestCase {
+        private Profile profile;
+        @Test
+        public void testNull() {
+            // check to see if profile is null
+            assertNull(profile);
+        }
+
+        // Set profile for each test with @Before to run before each test
+        @Before
+        public void setProfile() {
+            profile = new Profile("abaldocc", "whatsup", 20, "Male", "Salvadorian", "Building Manager", "Soccer");
+        } // end of setProfile
+
+        /*Testing of constructors and getters*/
+        @Test
+        public void profileTestGettersAndConstructor() {
+            assertEquals("Username should be abaldocc", "abaldocc", profile.getUserName()); // is username requiered
+            // firt
+            // letter capital? Is it case sensitive?
+            assertEquals("Password should be whatsup", "whatsup", profile.getPassword());
+            assertEquals("Age should be 20", 20, profile.getAge());
+            assertEquals("Gender should be Male", "Male", profile.getGender());
+            assertEquals("Nationality should be Salvadorian", "Salvadorian", profile.getNationality());
+            assertEquals("Job should be Building Manager", "Building Manager", profile.getJob());
+            assertEquals("Hobby should be Soccer", "Soccer", profile.getHobby());
+        }
+
+        @Test
+        public void profileSetterTest() {
+            profile.setUserName("alexia");
+            assertEquals("Username should now be alexia", "alexia", profile.getUserName());
+            profile.setPassword("whyy");
+            assertEquals("Password should now be whyy", "whyy", profile.getPassword());
+            profile.setAge(18);
+            assertEquals("Age should now be 18", 18, profile.getAge());
+            profile.setGender("Female");
+            assertEquals("Gender should now be Female", "Female", profile.getGender());
+            profile.setNationality("American");
+            assertEquals("Nationality should now be American", "American", profile.getNationality());
+            profile.setJob("Student");
+            assertEquals("Job should now be Student", "Student", profile.getJob());
+            profile.setHobby("Poker");
+            assertEquals("Hobby should now be Poker", "Poker", profile.getHobby());
+        }
+    } // end of test case
+} // end of class
