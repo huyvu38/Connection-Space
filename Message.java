e8e7aca7ad5188124d325ff85469f2256e2b04d4
turner474 <143348867+turner474@users.noreply.github.com>
2024-03-31 13:45:32 -0400

    update

--- Message.java
+++ Message.java
@@ -133,9 +133,12 @@ public class Message implements MessageInterface {
     }
 
 
+    // Only send message to selected members in friendList (before use this method, make sure all input should in Users friendList.
     // Only send message when otherUserName is in friendList
     // return empty string if all success, otherwirse indicate which one failed.
+    public  String restrictMessage(String userName, ArrayList<String> groupMembersList, String content) {
         List<String> failedUser = new ArrayList<>();
+        for (String friend: groupMembersList) {
             if(!this.sendMessage(userName, friend, content, false)) {
                 failedUser.add(friend);
             }
@@ -194,6 +197,7 @@ public class Message implements MessageInterface {
                 if (array[1].equals(0)
                         && array[3].equals(senderName)
                         && array[4].equals(receiverName)) {
+                    System.out.printf("%s %s %s: %s %s", array[0],array[2],array[3],array[6],array[5]);
                 }
             }
             //return true;
