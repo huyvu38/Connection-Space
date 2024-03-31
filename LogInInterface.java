070c4e0099ba46c70bd6759f046bbbcc47fbc810
huyvu38 <vu38@purdue.edu>
2024-03-31 12:37:41 -0400

    Merge remote-tracking branch 'origin/main'

--- LogInInterface.java
+++ LogInInterface.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
  * @version 28 March 2024
  */
 public interface LogInInterface {
-    boolean isValidUserName(ArrayList<Profile> allUserList, String userName);
+    static boolean isValidUserName(ArrayList<Profile> allUserList, String userName);
     boolean checkPasswordLength(String password);
     boolean checkIfPasswordCorrect(Profile profile, String password);
     boolean createAccount(Database database, Profile newProfile);
