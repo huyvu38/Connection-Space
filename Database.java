45c36db93ba1c7b9d1f5e6e160437a01cc38291e
huyvu38 <vu38@purdue.edu>
2024-03-31 21:43:11 -0400

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	SocialMedia.java

--- Database.java
+++ Database.java
@@ -15,7 +15,7 @@ public class Database implements DatabaseInterface {
     private ArrayList<Profile> allUserProfile;
     private ArrayList<UserAccount> allUserAccount;
 
-    public Database(String allUserAccountFile) {
+    public Database (String allUserAccountFile) {
         this.allUserAccountFile = allUserAccountFile;
         this.allUserAccount = new ArrayList<>();
         this.allUserProfile = new ArrayList<>();
@@ -27,12 +27,10 @@ public class Database implements DatabaseInterface {
             FileReader fr = new FileReader(f);
             BufferedReader bfr = new BufferedReader(fr);
             String line = bfr.readLine();
-
-            while (line != null) {
+            while(line != null) {
                 String[] element = line.split(";");
                 String[] userInfo = element[0].split(",");
-                Profile profile = new Profile(userInfo[0], userInfo[1], Integer.parseInt(userInfo[2]),
-                        userInfo[3], userInfo[4], userInfo[5], userInfo[6]);
+                Profile profile = new Profile(userInfo[0], userInfo[1], Integer.parseInt(userInfo[2]), userInfo[3], userInfo[4], userInfo[5], userInfo[6]);
                 allUserProfile.add(profile);
                 String friendList = element[1].substring(12, element[1].length() - 1);
                 ArrayList<String> friends = new ArrayList<>();
@@ -43,7 +41,7 @@ public class Database implements DatabaseInterface {
                 String blockList = element[2].substring(11, element[2].length() - 1);
                 ArrayList<String> blockusers = new ArrayList<>();
                 String[] eachBlockUser = blockList.split(" ");
-                for (String username : eachBlockUser) {
+                for (String username : blockusers) {
                     blockusers.add(username);
                 }
                 UserAccount userAccount = new UserAccount(profile);
@@ -51,11 +49,6 @@ public class Database implements DatabaseInterface {
                 userAccount.setBlockList(blockusers);
                 allUserAccount.add(userAccount);
                 line = bfr.readLine();
-                if (line.equals("-----END OF SAVE-----")) {
-                    if (bfr.readLine() != null) {
-                        allUserAccount.clear();
-                    }
-                }
             }
             bfr.close();
         } catch (FileNotFoundException e) {
@@ -77,7 +70,6 @@ public class Database implements DatabaseInterface {
                 pw.println(allUserAccount.get(i).toString());
             }
             pw.flush();
-            pw.println("-----END OF SAVE-----");
             pw.close();
 
         } catch (FileNotFoundException e) {
