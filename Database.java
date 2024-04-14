6521e1d2968f36d6a9692f6e15b4334e86d9234d
huyvu38 <vu38@purdue.edu>
2024-04-13 23:23:46 -0400

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	SocialMedia.java

--- Database.java
+++ Database.java
@@ -68,7 +68,7 @@ public class Database implements DatabaseInterface {
             pw.flush();
             pw.close();
 
-        } catch (FileNotFoundException e) {
+        } catch (Exception e) {
             return false;
         }
         return true;
